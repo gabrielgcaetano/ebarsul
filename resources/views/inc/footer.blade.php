8a4941cb086072aaaf934459c0cdb21425054884
Gabriel Caetano <gabriel.goulartcaetano@gmail.com>
2019-04-13 15:12:19 -0300

    - Colocado Logo no Rodapé

--- resources/views/inc/footer.blade.php
+++ resources/views/inc/footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="section back-black-dark">
     <div class="grid-34 offset-1 text-center">
-        {{--<a href="http://www.laluvia.com.br/" target="_blank">--}}
-        {{--<img class="fade img-center py-10" alt="Laluvia Front End Developer" src="assets/images/lgo/laluvia.png">--}}
-        {{--</a>--}}
+        <a href="http://www.spacecode.tech/" target="_blank">
+        <img class="fade img-center py-10" style="border-radius: 10px;" alt="Space Code" width="250px" src="{{ url('images/lgo/banner-space-code-rodape-small.png') }}">
+        </a>
     </div>
 </footer>
